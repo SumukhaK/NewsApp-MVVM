914320fd7ef6a7b03d44d47b54036b139343e136
sumukha <sumukh.aithal@gmail.com>
2025-01-07 23:11:44 +0530

    adds dependencies required for okhttp logs

--- app/src/main/java/com/ksa/newsapp_mvvm_architecture/di/module/ApplicationModule.kt
+++ app/src/main/java/com/ksa/newsapp_mvvm_architecture/di/module/ApplicationModule.kt
@@ -2,11 +2,16 @@ package com.ksa.newsapp_mvvm_architecture.di.module
 
 import android.app.Application
 import com.ksa.newsapp_mvvm_architecture.NewsApplication
+import com.ksa.newsapp_mvvm_architecture.data.api.NetworkService
 import com.ksa.newsapp_mvvm_architecture.di.ApplicationContext
 import com.ksa.newsapp_mvvm_architecture.di.BaseURL
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -22,12 +27,29 @@ class ApplicationModule(private val application: NewsApplication) {
     @Provides
     @Singleton
     fun provideGsonConverterFactory(): GsonConverterFactory
-    = GsonConverterFactory.create()
+            = GsonConverterFactory.create()
+
+    @Provides
+    @Singleton
+    fun getOkHttpBuilder():OkHttpClient{
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val httpClient = OkHttpClient()
+        val builder = httpClient.newBuilder()
+        builder.connectTimeout(20, TimeUnit.SECONDS)
+        builder.readTimeout(20, TimeUnit.SECONDS)
+        return builder.build()
+    }
 
     @Provides
     @Singleton
     fun provideNetworkService(@BaseURL baseURL: String,
-                              gsonConverterFactory: GsonConverterFactory){
-
+                              gsonConverterFactory: GsonConverterFactory) : NetworkService{
+        return Retrofit.Builder()
+            .baseUrl(baseURL)
+            .addConverterFactory(gsonConverterFactory)
+            .client(getOkHttpBuilder())
+            .build()
+            .create(NetworkService::class.java)
     }
 }
